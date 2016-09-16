66845244903deaf9cdb106e366f3658822f91a24
Vikontte <slyflut@gmail.com>
2016-09-16 23:21:32 +0300

    Выполнено задание #26 Контексты и прототипы

--- src/gallery.js
+++ src/gallery.js
@@ -10,46 +10,50 @@ define(function() {
       this.controlLeft = this.galleryContainer.querySelector('.overlay-gallery-control-left');
       this.controlRight = this.galleryContainer.querySelector('.overlay-gallery-control-right');
       this.imageContainer = this.galleryContainer.querySelector('.overlay-gallery-preview');
-
       this.pictures = pictures;
       this.activePicture = 0;
+
+      this.onControlLeftClick = this.onControlLeftClick.bind(this);
+      this.onControlRightClick = this.onControlRightClick.bind(this);
+      this.onCloseElementClick = this.onCloseElementClick.bind(this);
     };
 
-    // Методы
     Gallery.prototype.show = function(number) {
-      var self = this;
-      this.controlRight.onclick = function() {
-        number++;
-        if (number > 5) {
-          number = 5;
-        }
-        self.onControlClick(number);
-      };
-      this.controlLeft.onclick = function() {
-        number--;
-        if (number < 0) {
-          number = 0;
-        }
-        self.onControlClick(number);
-      };
-      this.closeElement.onclick = function() {
-        self.onCloseElementClick();
-      };
+      this.activePicture = number;
+      this.controlRight.addEventListener('click', this.onControlRightClick);
+      this.controlLeft.addEventListener('click', this.onControlLeftClick);
+      this.closeElement.addEventListener('click', this.onCloseElementClick);
+
       this.galleryContainer.classList.remove('invisible');
-      this.setActivePicture(number);
+      this.setActivePicture();
+    };
+
+    Gallery.prototype.onControlLeftClick = function() {
+      this.activePicture--;
+      if (this.activePicture < 0) {
+        this.activePicture = 0;
+      }
+      this.setActivePicture();
+    };
+
+    Gallery.prototype.onControlRightClick = function() {
+      this.activePicture++;
+      if (this.activePicture > 5) {
+        this.activePicture = 5;
+      }
+      this.setActivePicture();
     };
 
     Gallery.prototype.hide = function() {
-      this.closeElement.onclick = null;
-      this.controlRight.onclick = null;
-      this.controlLeft.onclick = null;
+      this.controlRight.removeEventListener('click', this.onControlRightClick);
+      this.controlLeft.removeEventListener('click', this.onControlLeftClick);
+      this.closeElement.removeEventListener('click', this.onCloseElementClick);
 
       this.galleryContainer.classList.add('invisible');
       this.imageContainer.removeChild(this.galleryImage);
     };
 
-    Gallery.prototype.setActivePicture = function(number) {
-      this.activePicture = number;
+    Gallery.prototype.setActivePicture = function() {
       this.galleryImage = new Image();
       this.galleryImage.src = this.pictures[this.activePicture];
       var previousImage = this.imageContainer.querySelector('img');
@@ -59,11 +63,6 @@ define(function() {
         this.imageContainer.replaceChild(this.galleryImage, previousImage);
       }
       this.imageContainer.querySelector('.preview-number-current').textContent = this.activePicture + 1;
-
-    };
-    // События
-    Gallery.prototype.onControlClick = function(number) {
-      this.setActivePicture(number);
     };
 
     Gallery.prototype.onCloseElementClick = function() {
